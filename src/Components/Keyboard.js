4b25e9aa0a343e384c6777daf59b0e83a6dfdb2f
Matt Carlyon <122873665+MattCarlyon@users.noreply.github.com>
2023-04-28 16:32:39 +0200

    commit before merge

--- src/Components/Keyboard.js
+++ src/Components/Keyboard.js
@@ -1,29 +1,28 @@
 import React from "react";
 import "./keyboard.css";
-import "./Key.css";
-//import Key from "./Keys"
+import Key from "./Keys"
 
 function Keyboard() {
     //Key Array
     const keys1 = ["Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P"];
     const keys2 = ["A", "S", "D", "F", "G", "H", "J", "K", "L"];
-    const keys3 = ["Z", "X", "C", "V", "B", "N", "M"];
+    const keys3 = ["Enter", "Z", "X", "C", "V", "B", "N", "M", "Delete"];
 
     //A different div for each of the keyboard lines. I have done this beacuse they have different sizes and therefore layout properties
     return <div className="Keyboard">
         <div className="row1">
             {keys1.map((key) => {
-                return <div className="key"> {key} </div>;
+                return <Key keyValue={key} />;
             })}
         </div>
         <div className="row2">
             {keys2.map((key) => {
-                return <div className="key"> {key} </div>;
+                return <Key keyValue={key} />;
             })}
         </div>
         <div className="row3">
             {keys3.map((key) => {
-                return <div className="key"> {key} </div>;
+                return <Key keyValue={key} />;
             })}
         </div>
         <div className="row2"></div>
--- src/Components/Keys.js
+++ src/Components/Keys.js
@@ -1,8 +1,11 @@
 import React from "react"; 
 import "./Key.css";
 
-function Key({keyvalue}) {
-    return <div className="key">{keyvalue}</div>
-};
+function Key({keyValue}) {
+    const selectLetter = () => {
+    }
+    return (
+        <div className="key" onClick={selectLetter}>{keyValue}</div>
+)};
 
 export default Key;
