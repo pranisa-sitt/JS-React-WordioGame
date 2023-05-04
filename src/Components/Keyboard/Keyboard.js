2e8c78485139d61992b0a2ff7d45cd3d55c239b9
Joana Brito <wizardjoana@gmail.com>
2023-05-04 12:55:24 +0100

    helena coaching

--- src/Components/Keyboard/Keyboard.js
+++ src/Components/Keyboard/Keyboard.js
@@ -2,31 +2,34 @@ import React from "react";
 import "./keyboard.css";
 import Key from "./Keys"
 
-function Keyboard() {
+function Keyboard({selectLetter, keys1, color}) {
     //Key Array
-    const keys1 = ["Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P"];
-    const keys2 = ["A", "S", "D", "F", "G", "H", "J", "K", "L"];
-    const keys3 = ["Enter", "Z", "X", "C", "V", "B", "N", "M", "Delete"];
+   // const keys1 = ["Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P"];
+    //const keys2 = ["A", "S", "D", "F", "G", "H", "J", "K", "L"];
+    //const keys3 = ["Enter", "Z", "X", "C", "V", "B", "N", "M", "Delete"];
 
-    //A different div for each of the keyboard lines. I have done this beacuse they have different sizes and therefore layout properties
+    //create 3 different keyboard components
+
     return <div className="Keyboard">
         <div className="row1">
-            {keys1.map((key) => {
-                return <Key keyValue={key} />;
-            })}
+            {keys1.map((key) => (
+                <Key color={color} keyValue={key} selectLetter={selectLetter} />
+            ))};
         </div>
+   
+        {/*
         <div className="row2">
-            {keys2.map((key) => {
-                return <Key keyValue={key} />;
-            })}
+            {keys2.map((key) => (
+                <Key keyValue={key} selectLetter={selectLetter} />
+            ))}
         </div>
         <div className="row3">
             {keys3.map((key) => {
-                return <Key keyValue={key} />;
+                return <Key keyValue={key} selectLetter={selectLetter} />;
             })}
         </div>
         <div className="row2"></div>
-        <div className="row3"></div>
+        <div className="row3"></div> */}
     </div>;
 };
 
