2d91ab5f3ad99e268c2e7b553d589a81312b114c
Matt Carlyon <122873665+MattCarlyon@users.noreply.github.com>
2023-05-04 14:42:54 +0200

    3 keyboard functions added

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
-import Keyboard from "./Components/Keyboard/Keyboard.js";
+import Keyboard1 from "./Components/Keyboard/Keyboard1.js";
+import Keyboard2 from "./Components/Keyboard/Keyboard1.js";
+import Keyboard3 from "./Components/Keyboard/Keyboard1.js";
 import Grid from "./Components/Grid/Grid.js";
 import { useState } from "react";
 
 const keys1 = ["Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P"];
+const keys2 = ["A", "S", "D", "F", "G", "H", "J", "K", "L"];
+const keys3 = ["Enter", "Z", "X", "C", "V", "B", "N", "M", "Delete"];
 
 
 
@@ -11,16 +15,16 @@ const keys1 = ["Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P"];
 
 function App() {
 
-  const [keys, setKeys] = useState(keys1);
-
-
+  const [keysOne, setKeys] = useState(keys1);
+  const [keysTwo, setKeys] = useState(keys2);
+  const [keysThree, setKeys] = useState(keys3);
 
   return (
     <div>
       <Grid></Grid>
-      <Keyboard keys1={keys} selectLetter={() => console.log("hello")} />
-
-      
+      <Keyboard1 keys1={keysOne} selectLetter={() => console.log("hello1")} />
+      <Keyboard2 keys2={keysTwo} selectLetter={() => console.log("hello2")} />
+      <Keyboard3 keys3={keysThree} selectLetter={() => console.log("hello3")} />
     </div>
   );
 }
--- src/Components/Keyboard/Keyboard1.js
+++ src/Components/Keyboard/Keyboard1.js
@@ -2,7 +2,7 @@ import React from "react";
 import "./keyboard.css";
 import Key from "./Keys"
 
-function Keyboard({selectLetter, keys1, color}) {
+function Keyboard1({selectLetter, keys1, color}) {
     //Key Array
    // const keys1 = ["Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P"];
     //const keys2 = ["A", "S", "D", "F", "G", "H", "J", "K", "L"];
@@ -11,26 +11,12 @@ function Keyboard({selectLetter, keys1, color}) {
     //create 3 different keyboard components
 
     return <div className="Keyboard">
-        <div className="row1">
+        <div className="Row">
             {keys1.map((key) => (
                 <Key color={color} keyValue={key} selectLetter={selectLetter} />
             ))};
         </div>
-   
-        {/*
-        <div className="row2">
-            {keys2.map((key) => (
-                <Key keyValue={key} selectLetter={selectLetter} />
-            ))}
-        </div>
-        <div className="row3">
-            {keys3.map((key) => {
-                return <Key keyValue={key} selectLetter={selectLetter} />;
-            })}
-        </div>
-        <div className="row2"></div>
-        <div className="row3"></div> */}
     </div>;
 };
 
-export default Keyboard;
+export default Keyboard1;
