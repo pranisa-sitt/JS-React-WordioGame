86e7527a78a6d0babb7d832db82920feb5a8218d
Joana Brito <wizardjoana@gmail.com>
2023-05-04 16:41:13 +0100

    add changes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
-import Keyboard from "./Components/Keyboard/Keyboard.js";
+import Keyboard1 from "./Components/Keyboard/Keyboard1.js";
+import Keyboard2 from "./Components/Keyboard/Keyboard2.js";
+import Keyboard3 from "./Components/Keyboard/Keyboard3.js";
 import Grid from "./Components/Grid/Grid.js";
 import { useState } from "react";
 import RowOne from "./Components/Grid/Rows/RowOne.js";
 
-const keys1 = ["Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P"]
+const keys1 = ["Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P"];
+const keys2 = ["A", "S", "D", "F", "G", "H", "J", "K", "L"];
+const keys3 = ["Enter", "Z", "X", "C", "V", "B", "N", "M", "Delete"];
 const row1 = ["", "", "", "", ""]
 
 
@@ -13,8 +17,11 @@ const row1 = ["", "", "", "", ""]
 
 function App() {
 
-  const [keys, setKeys] = useState(keys1);
-  const [row, setRow] = useState(row1);
+    const [keysOne] = useState(keys1);
+    const [keysTwo] = useState(keys2);
+    const [keysThree] = useState(keys3);
+
+    const [row, setRow] = useState(row1);
 
 
 
@@ -27,9 +34,9 @@ function App() {
   return (
     <div>
       <RowOne box={row} />
-      <Keyboard keys1={keys} selectLetter={() => console.log(addLetter)} />
-
-      
+      <Keyboard1 keys1={keysOne} selectLetter={() => console.log("hello1")} />
+      <Keyboard2 keys2={keysTwo} selectLetter={() => console.log("hello2")} />
+      <Keyboard3 keys3={keysThree} selectLetter={() => console.log("hello3")} />
     </div>
   );
 }
