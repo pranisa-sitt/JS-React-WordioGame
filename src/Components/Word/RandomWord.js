3855b4dcd3daca656dd961fbd9760816a59e5912
pranisa-sitt <pranisa.sitt@gmail.com>
2023-05-08 03:27:00 +0200

    Get random word function

---
+++ src/Components/Word/RandomWord.js
@@ -0,0 +1,24 @@
+
+const RandomWord = () => {
+  const words = [
+    ['a','g','a','i','n'],
+    ['b','a','c','o','n'],
+    ['b','r','a','i','n'],
+    ['c','a','m','e','l'],
+    ['d','a','n','c','e']
+  ];
+
+  const getRandomWord = () => {  
+    return   words[Math.floor(Math.random()*words.length)];
+  };
+
+
+  return (
+    <div>
+    {getRandomWord}
+    {console.log(getRandomWord())}
+    </div>
+  );
+};
+
+export default RandomWord;
