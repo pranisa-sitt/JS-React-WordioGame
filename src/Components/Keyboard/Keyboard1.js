6f0f55700f4bd2adda9b53a8774fbb079d35621e
Matt Carlyon <122873665+MattCarlyon@users.noreply.github.com>
2023-05-11 11:00:33 +0200

    letters added to grid
    del and enter buttons to do

--- src/Components/Keyboard/Keyboard1.js
+++ src/Components/Keyboard/Keyboard1.js
@@ -2,7 +2,7 @@ import React from "react";
 import "./keyboard.css";
 import Key from "./Keys"
 
-function Keyboard1({selectLetter, keys1, color, currentSquare}) {
+function Keyboard1({selectLetter, keys1, color, currentSquare, addLetter}) {
     // console.log(keys1);
     //Key Array
    // const keys1 = ["Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P"];
@@ -13,7 +13,7 @@ function Keyboard1({selectLetter, keys1, color, currentSquare}) {
     return <div>
         <div className="Row">
             {keys1.map((key) => (
-                <Key color={color} keyValue={key.keyVal} key={key.id} selectLetter={key.keyVal} currentSquare={currentSquare} />
+                <Key color={color} keyValue={key.keyVal} key={key.id} selectLetter={key.keyVal} currentSquare={currentSquare} addLetter={addLetter}/>
             ))}
         </div>
     </div>;
