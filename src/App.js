937e8b88b1e60c1e60928b01677bcafa9fc0d429
Matt Carlyon <122873665+MattCarlyon@users.noreply.github.com>
2023-05-16 14:11:55 +0200

    Keyboard working and grid fixed

--- src/App.js
+++ src/App.js
@@ -18,12 +18,76 @@ const keys1 = [
   { keyVal: "P", id: 10 },
 ];
 
+const keys2 = [
+  { keyVal: "A", id: 11 },
+  { keyVal: "S", id: 12 },
+  { keyVal: "D", id: 13 },
+  { keyVal: "F", id: 14 },
+  { keyVal: "G", id: 15 },
+  { keyVal: "H", id: 16 },
+  { keyVal: "J", id: 17 },
+  { keyVal: "K", id: 18 },
+  { keyVal: "L", id: 19 },
+];
+
+const keys3 = [
+  { keyVal: "DEL", id: 20 },
+  { keyVal: "Z", id: 21 },
+  { keyVal: "X", id: 22 },
+  { keyVal: "C", id: 23 },
+  { keyVal: "V", id: 24 },
+  { keyVal: "B", id: 25 },
+  { keyVal: "N", id: 26 },
+  { keyVal: "M", id: 27 },
+  { keyVal: "ENT", id: 28 },
+];
+
 const row1 = [
   { rowVal: "", id: 29 },
   { rowVal: "", id: 30 },
   { rowVal: "", id: 31 },
   { rowVal: "", id: 32 },
   { rowVal: "", id: 33 },
+];
+
+const row2 = [
+  { rowVal: "", id: 33 },
+  { rowVal: "", id: 34 },
+  { rowVal: "", id: 35 },
+  { rowVal: "", id: 36 },
+  { rowVal: "", id: 37 },
+];
+
+const row3 = [
+  { rowVal: "", id: 39 },
+  { rowVal: "", id: 40 },
+  { rowVal: "", id: 41 },
+  { rowVal: "", id: 42 },
+  { rowVal: "", id: 43 },
+];
+
+const row4 = [
+  { rowVal: "", id: 44 },
+  { rowVal: "", id: 45 },
+  { rowVal: "", id: 46 },
+  { rowVal: "", id: 47 },
+  { rowVal: "", id: 48 },
+];
+
+const row5 = [
+  { rowVal: "", id: 49 },
+  { rowVal: "", id: 50 },
+  { rowVal: "", id: 51 },
+  { rowVal: "", id: 52 },
+  { rowVal: "", id: 53 },
+];
+
+const row6 = [
+  { rowVal: "", id: 54 },
+  { rowVal: "", id: 55 },
+  { rowVal: "", id: 56 },
+  { rowVal: "", id: 57 },
+  { rowVal: "", id: 58 },
 ];
 
 const words = [
@@ -35,19 +99,25 @@ const words = [
 ];
 
 function App() {
-  const [keys, setKeys] = useState(keys1);
+  const [keys, setKeys] = useState([keys1, keys2, keys3]);
 
   let [currentIndex, setCurrentIndex] = useState(0);
   const [currentSquare, setCurrentSquare] = useState(row1[currentIndex]);
   const [currentRowIndex, setCurrentRowIndex] = useState(0);
 
-  const [rows, setRows] = useState([row1]);
+  const [rows, setRows] = useState([row1, row2, row3, row4, row5, row6]);
 
   const [color, setColor] = useState("#E949494");
 
   const [word, setWord] = useState([]);
 
   const [rowOne, setRowOne] = useState(row1);
+  const [rowTwo, setRowTwo] = useState(row2);
+  const [rowThree, setRowThree] = useState(row3);
+  const [rowFour, setRowFour] = useState(row4);
+  const [rowFive, setRowFive] = useState(row5);
+  const [rowSix, setRowSix] = useState(row6);
+
 
   useEffect(() => {
     randomWord();
@@ -100,10 +170,21 @@ function App() {
 
       <div className="grid-container">
         <Row rows={rowOne} word={word} />
+        <Row rows={rowTwo} word={word} />
+        <Row rows={rowThree} word={word} />
+        <Row rows={rowFour} word={word} />
+        <Row rows={rowFive} word={word} />
+        <Row rows={rowSix} word={word} />
       </div>
       <div className="Keyboard">
         <div>
-          <Keyboard color={color} keys={keys} handleKeyPress={handleKeyPress} />
+          <Keyboard color={color} keys={keys1} handleKeyPress={handleKeyPress} />
+        </div>
+        <div>
+          <Keyboard color={color} keys={keys2} handleKeyPress={handleKeyPress} />
+        </div>
+        <div>
+          <Keyboard color={color} keys={keys3} handleKeyPress={handleKeyPress} />
         </div>
       </div>
     </div>
